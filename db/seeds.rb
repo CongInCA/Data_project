77f8dda0af5fc2435f7422ecf9f2ca90e0bb5041
CongInCA <captainchenn@163.com>
2024-03-16 12:15:20 -0500

    Create data import

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,42 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Import data from API
+require 'net/http'
+require 'json'
+
+uri = URI('https://data.winnipeg.ca/resource/gp3k-am4u.json')
+response = Net::HTTP.get(uri)
+transit_data = JSON.parse(response)
+
+transit_data.each do |data|
+    day_type = data['day_type']
+    day = DayType.find_or_create_by(day: day_type)
+
+  TransitPerformance.create(
+    driver_id: Faker::Number.unique.between(from: 1, to: 10),
+    day_id: day.id,
+    stop_number: data['stop_number'],
+    route_number: data['route_number'],
+    route_name: data['route_name'],
+    route_destination: data['route_destination'],
+    scheduled_time: DateTime.parse(data['scheduled_time']),
+    deviation: data['deviation']
+  )
+end
+
+# Import data from faker
+10.times do
+    Driver.create(
+      name: Faker::Name.name,
+      license_number: Faker::IDNumber.unique.valid
+    )
+  end
+  
+# Import data from CSV
+require 'csv'
+
+CSV.foreach('path/to/day_types.csv', headers: true) do |row|
+DayType.create(day_id: row['day_id'], day: row['day'])
+end
